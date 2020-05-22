75bc039b3899933d419fb20234dca3ca93e251a3
Isaac Yao Tuagba <calculus2uk@gmail.com>
2020-05-23 01:06:24 +0300

    Created User and Pin Models

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 //typeDefs and Resolvers
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const MONGODB_URL = process.env.MONGODB_URL;
+
+//DB CONNECTION
+mongoose
+	.connect(MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log(' 🚀 DB Successfully connected 🚀  '))
+	.catch(() => console.log('  💥 💣 DB Connection failed  💥'));
 
 // The ApolloServer constructor
 const server = new ApolloServer({ typeDefs, resolvers });
--- typeDefs.js
+++ typeDefs.js
@@ -4,11 +4,32 @@ module.exports = gql`
 	#USER TYPE
 	type User {
 		_id: ID
-		name: String!
-		email: String!
+		name: String
+		email: String
 		picture: String
 	}
 
+	#type PIN
+	type Pin {
+		_id: ID
+		createdAt: String
+		title: String
+		content: String
+		image: String
+		latitude: Float
+		longitude: Float
+		author: User
+		comments: [Comment]
+	}
+
+	#type PIN
+	type Comment {
+		text: String
+		createdAt: String
+		author: User
+	}
+
+	#QUERY
 	type Query {
 		me: User!
 	}
