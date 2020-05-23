3851add05317b8be2cf6428bf24108e9a7b42524
Isaac Yao Tuagba <calculus2uk@gmail.com>
2020-05-23 21:44:51 +0300

    Added Google Login Button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { GoogleLogin } from 'react-google-login';
 // import Typography from "@material-ui/core/Typography";
 
+const clientId = process.env.REACT_APP_GEOPINS_GOOGLE_ID;
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+	const onSuccess = (googleUser) => {
+		const idToken = googleUser.getAuthResponse().id_token;
+	};
+	return (
+		<GoogleLogin clientId={clientId} onSuccess={onSuccess} isSignedIn='true' />
+	);
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+	root: {
+		height: '100vh',
+		display: 'flex',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
 };
 
 export default withStyles(styles)(Login);
